53c496924987cdd8bc4866f884557157d5269bcc
github username <oliviatian27@gmail.com>
2019-02-03 23:48:52 -0500

    property-binding

--- my-app/src/app/server/server.component.ts
+++ my-app/src/app/server/server.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'app-server',
-  templateUrl:
+  templateUrl:'./server.component.html'
 })
 
 export class ServerComponent{
+  serverId:number=10;
+  serverStatus:string='offline';
 
+  getServerStatus(){
+    return this.serverStatus
+  }
 }
