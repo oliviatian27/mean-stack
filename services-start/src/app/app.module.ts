cf0cb22f193d6db18d0a3a94698cace103061454
github username <oliviatian27@gmail.com>
2019-02-08 10:29:36 -0500

    service-final

--- services-start/src/app/app.module.ts
+++ services-start/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
-import { AccountsService } from './accounts.service'
-import { LoggingService } from './logging.service'
 
 @NgModule({
   declarations: [
@@ -20,7 +18,6 @@ import { LoggingService } from './logging.service'
     FormsModule,
     HttpModule
   ],
-  providers:[AccountsService,LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
