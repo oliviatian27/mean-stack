245a09bdd973e086fa37713585aa690f509fbcea
github username <oliviatian27@gmail.com>
2019-02-12 23:50:03 -0500

    nested-routes-2

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {Routes,RouterModule} from '@angular/router'
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,17 +12,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from "./app-routing.module";
 
-const appRoutes:Routes=[
-  {path:'',component:HomeComponent},
-  {path:'servers',component:ServersComponent,children:[
-        {path:':id',component:ServerComponent},
-      {path:':id/edit',component:EditServerComponent}
-    ]},
-  {path:'users',component:UsersComponent,children:[
-      {path:':id/:name',component:UserComponent}
-    ]},
-]
 
 @NgModule({
   declarations: [
@@ -32,13 +24,14 @@ const appRoutes:Routes=[
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
