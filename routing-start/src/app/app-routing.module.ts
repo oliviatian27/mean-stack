16ec304caa3ea2e742f686856103fba0bb35f7a6
github username <oliviatian27@gmail.com>
2019-02-23 23:34:56 -0500

    resolve guard

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -8,9 +8,10 @@ import {ServersComponent} from './servers/servers.component';
 import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {AuthGuard} from "./auth-guard.service";
 import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
+import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerResolver} from "./servers/server/server-resolver.service";
 
 const appRoutes:Routes=[
   {path:'',component:HomeComponent},
@@ -18,13 +19,14 @@ const appRoutes:Routes=[
     // canActivate:[AuthGuard],
     canActivateChild:[AuthGuard],
     component:ServersComponent,children:[
-      {path:':id',component:ServerComponent},
+      {path: ':id', component: ServerComponent, resolve: {server: ServerResolver}},
       {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
     ]},
   {path:'users',component:UsersComponent,children:[
       {path:':id/:name',component:UserComponent}
     ]},
-  {path:'not-found',component:PageNotFoundComponent},
+  // {path:'not-found',component:PageNotFoundComponent},
+  {path: 'not-found', component: ErrorPageComponent, data: {message: "page not found"}},
   {path:'**',redirectTo:'/not-found'}
 ];
 @NgModule({
