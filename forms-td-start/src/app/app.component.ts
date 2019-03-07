c302e9370f713b91c1deaec3190b30bc97e01097
github username <oliviatian27@gmail.com>
2019-03-06 23:18:28 -0500

    form-start

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm:NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+  // onSubmit(f:NgForm){
+  //   //    console.log(f)
+  //   // }
+
+  onSubmit(){
+    console.log(this.signupForm)
   }
 }
