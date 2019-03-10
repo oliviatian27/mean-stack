8f243ff125e65eeeb93734f8232864cce1d390b7
github username <oliviatian27@gmail.com>
2019-03-09 23:45:54 -0500

    patch-value

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,9 +9,28 @@ import {NgForm} from "@angular/forms";
 export class AppComponent {
   @ViewChild('f') signupForm:NgForm;
   defaultQuestion="pet";
+  answer: string;
+  genders=['male','female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue(
+    //   {userData:{
+    //     username:suggestedName,
+    //       email:''
+    // },
+    //     secret:'pet',
+    //     questionAnswer:'',
+    //     gender:'male'
+    //   }
+    // )
+    this.signupForm.form.patchValue(
+      {
+        userData:{
+          username:suggestedName
+        }
+      }
+    )
   }
   // onSubmit(f:NgForm){
   //   //    console.log(f)
