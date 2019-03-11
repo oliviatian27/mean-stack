16d6ab331790b1ac24339d3ea550ac0a11749333
olivia <oliviatian27@gmail.com>
2019-03-10 23:32:39 -0400

    Merge branch 'master' of https://github.com/oliviatian27/mean-stack
    # Conflicts:
    #	.idea/workspace.xml

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
 
   answer: string;
   genders=['male','female'];
+  user ={
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  };
+  submitted=false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -38,6 +46,12 @@ export class AppComponent {
   //   // }
 
   onSubmit(){
-    console.log(this.signupForm)
+    this.submitted=true;
+    this.user.username=this.signupForm.value.userData.username;
+    this.user.email=this.signupForm.value.userData.email;
+    this.user.secretQuestion=this.signupForm.value.secretQuestion;
+    this.user.answer=this.signupForm.value.answer;
+    this.user.gender=this.signupForm.value.gender;
+    this.signupForm.reset();
   }
 }
