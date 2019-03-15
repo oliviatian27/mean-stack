9134de82dce0ef527121fd6cebd4c7af735f5375
github username <oliviatian27@gmail.com>
2019-03-14 23:18:21 -0400

    proj-form-start-validator

--- recipe/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,ViewChild,ElementRef,EventEmitter,Output } from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model'
 import {ShoppingListService} from '../shopping-list.service'
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -17,9 +18,10 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddItem(){
-    const newIngredient= new Ingredient(this.nameInputRef.nativeElement.value,this.amountInputRef.nativeElement.value)
+  onAddItem(form: NgForm) {
+    // const newIngredient= new Ingredient(this.nameInputRef.nativeElement.value,this.amountInputRef.nativeElement.value)
     // this.ingredientAdded.emit(newIngredient)
+    const newIngredient = new Ingredient(form.value.name, form.value.amount)
     this.slService.addIngredient(newIngredient)
 
   }
